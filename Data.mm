bcd25feb0d054ed6891daf2dedd65923a28ef0ae
Mark Bennett <mbenn13@uw.edu>
2021-03-05 16:44:58 -0800

    part1 completion
    data now computes all necessary information to produce the monodromy group. Code refactored

--- Data.mm
+++ Data.mm
@@ -1,62 +1,230 @@
 Data:= proc()
-   local this;
+   local this; uses ListTools, GraphTheory, plots;
    this := module()
-      export setSites, setVertex, setSegment, setRay, setLine, getData, cellData:=table(),edgeSiteData:=table(), vertices, edges:={};
-      local generateCell, generateCells, curves := {}, temp, e, s;
+# exported procedures/modules
+      export setSites, setVertex, setSegment, setRay, setLine, getData,
+      # exported variables
+       edges:= {}, vertices := {}, cellData := table(), vertexEdgeData := table(), edgeSiteData := table(), sitesOfDiagram:={}, cycleData := table();
+      # local procedures/modules
+      local generateCell, generateCells, canonicalVert, produceCycle,
+      # local vars
+      curves := {}, e, s, G;
 
-
+      # sets the sites of the diagram. Expected input is a set and since users do not access it, there is no need to catch errors
       setSites := proc(site) 
-         vertices := site;
+         sitesOfDiagram := site;
          return this;
       end proc;
 
-      
+      # sets the sites of the diagram. Expected input is a set and since users do not access it, there is no need to catch errors      
       setSegment := proc(x1,y1,x2,y2,s1,s2) 
+      local temp;
          temp:=[[x1,y1],[x2,y2]];
          curves := curves union {temp};
-         edges:=edges union {temp};
-         edgeSiteData[temp]:={s1,s2};
+         edges:=edges union {{op(temp)}};
+         edgeSiteData[{op(temp)}]:={s1,s2};
+         vertices := vertices union { temp[1], temp[2] };
+         if assigned( vertexEdgeData[ temp[1]] ) then 
+            vertexEdgeData[ temp[1]] :=  vertexEdgeData[ temp[1]] union { {op(temp)} };
+            #print("there should be two");
+            #print(vertexEdgeData[pt1]);
+         else
+            vertexEdgeData[ temp[1]] :=  { {op(temp)} };
+         fi;
+         if assigned( vertexEdgeData[ temp[2]] ) then 
+            vertexEdgeData[ temp[2] ] :=  vertexEdgeData[temp[2]] union { {op(temp)} };
+         else
+            vertexEdgeData[temp[2]] :=  {{op(temp)} };
+         fi;
+
          return this;
       end proc;
+      #Determines vertex to be called canonical
+      #In our case, we will take a the vertex with the smallest x value, and then the smallest y
+      #as we said above this is not user interactive so we need not catch errors
+      canonicalVert := proc(V)
+         local w ,v := V[1]; #start with an arbitrary vert
+         #print("the canonical vert for");
+         #print(V);
+         #print("is");
+         for w in V do
+            # if both verts of the edge have a larger x then nothing to do
+            if w[1] > v[1] then
+               break;
+            else
+            #otherwise if the first vert either has smaller x val or the same x val and smaller y replace
+               if ( w[1] = v[1] and w[2] < v[2] ) or w[1] < v[1]  then
+                  v := w;
+               end;
+            end;               
+         od;
+         #print(v);
+         return v;
+      end proc;
+   #Takes a vert of the graph and makes a cycle out of it. The graph in question has obvious constraints on it in the context of what we are doing
+      produceCycle := proc( pt, E);
+         local crtEdge, crtPt, nxtEdge, nxtPt, walk := [], auxEdgeSet, auxEdge, auxSet;
+         #print("producing cycle for");
+         #print(pt);
+         #print("with ");
+         
+         #intialize: We need a current edge, we need to know the direction is correct.
+         auxEdgeSet := E; #we need a set to remove edges from
+         #print(auxEdgeSet);
+         auxSet := E intersect vertexEdgeData[pt]; # need to start with set of edges that contain the starting pt
+      
+         #print("first set of edges COMES FROM"         );
+         #print(vertexEdgeData[pt]);
+         #print("and is");
+         #print(auxSet);
+         crtPt := pt; #starting pt is current pt
+         # for the curent edge we have two choices and only one is correct. The choice of pt is cannoical (so smallest x, smallest y). This and a simple argument about lines show that the correct direction
+         # will be obtained (CCW of course) by taking the edge with the property that the other end pt has the smallest y value
+         
+         #if the first edge in this set has ending vert of y value less than the ending vert of the others y value it is the edge we need
+         if op( auxSet[1] minus {crtPt})[2]< op( auxSet[2] minus {crtPt})[2] then
+            crtEdge:= [crtPt,op( (auxSet[1] minus {crtPt}) )]; #need to start with one of the two choices for edge paths. it must start at pt. because edge data is a set the end pt is just the arbitrary edge minus the start pt. We convert this to a directed edge.
+         else
+            crtEdge:= [crtPt,op( (auxSet[2] minus {crtPt}) )];
+         fi;
+         
+         #SCRATCH
+         #crtEdgeVector := <crtEdge[2][1]-crtEdge[1][1], crtEdge[2][2]-crtEdge[1][2]>; #used to calculate orientation of path
+         #assuming that pt is chosen in the canonical way, the direction we must travel is in the positive x and negative y. More testing needs to be done, but this appears to be a charachterization
+         #if not( crtEdgeVector[1]>0 and crtEdgeVector[2]<0) then 
+         #   crtEdge:= [crtPt,op( (auxSet[2]) minus {crtPt})];
+         #   crtEdgeVector := <crtEdge[2][1]-crtEdge[1][1], crtEdge[2][2]-crtEdge[1][2]>;
+         #fi;
+         
+         walk := [crtEdge];
+         #Intialization is complete
+         #print("starting walk");
+         #print(walk);
+         auxEdgeSet := auxEdgeSet minus { {op(crtEdge)} }; #the directed edge now needs to be converted and then removed from our set of edges
+         #print(auxEdgeSet);
+         # we can now inductively continue. while the set is not empty, choose the two set collection of edges that correspond to the nxt pt. The current edge is in this set so remove it to determine the direction
+         # we are traveling. then update location and remove the edge from our collection.
+         while not( auxEdgeSet = {} ) do
+            nxtPt := crtEdge[2]; #travel down edge
+         #   print(nxtPt);
+         #   print( auxEdgeSet intersect vertexEdgeData[ nxtPt]);
+            auxEdge := op( auxEdgeSet intersect vertexEdgeData[ nxtPt ] ); #there are only two edges that contain the nxt pt: the current edge and the one we need-we already removed the current edge
+            nxtEdge := [nxtPt, op( auxEdge minus { nxtPt } ) ] ; #edges are stored as {pt1, pt2} so this is op({pt})=pt
+            walk := [op(walk), nxtEdge];
+         #   print("updated walk");
+         #   print(walk);
+            #update location
+            crtPt := crtEdge[1];
+            crtEdge := nxtEdge;
+            #remove from list
+            auxEdgeSet := auxEdgeSet minus { {op(crtEdge)} }
+         od;       
+         return walk;
 
-      setRay := proc(x,y,dx,dy,s1,s2) 
-         local mult;
-         mult := range/evalf(sqrt(dx^2+dy^2)); 
-         curves := curves union {[[x,y],[x+mult*dx,y+mult*dy]]};
-         return this;
       end proc;
 
-      setLine := proc(x,y,dx,dy,s1,s2)
-         local mult;
-         mult := range/evalf(sqrt(dx^2+dy^2)); # ensure the line extends to the edges
-         curves := curves union {[[x-mult*dx,y-mult*dy],[x+mult*dx,y+mult*dy]]};
-         return this;
-      end proc;
 
+
+
+   #Determines vertex to be called canonical
+      #In our case, we will take a the vertex with the smallest x value, and then the smallest y
+      #as we said above this is not user interactive so we need not catch errors
+      
+
+     
+     
+   
+      # for a site collect edges that form cell. Take verts of cell and choose canonical one. Then arrange edges as directed cycle starting at canonical choice. The output is a ordered tuple: the canonical pt, and a ordered tuple that represents the path around the site.
       generateCell := proc(site)
-         local auxSet:={};
-           
+         local auxSet := {}, auxSet1 := {}, auxSet2 := {}, pt;
+         #print(edges);
+         #print("generating cell for");
+         #print(site);
          for e in edges do
+         #print("edge data");
+         #print(e);
+         #print(edgeSiteData[e]);
+         #print(evalb(site in edgeSiteData[e]) );
             if site in edgeSiteData[e] then
                auxSet := {e} union auxSet;
+               auxSet1 := {[op(e)]} union auxSet1;
+               auxSet2 := {op(e)} union auxSet2;
             fi;
          od;
-         cellData[site]:=auxSet;
+         pt := canonicalVert(auxSet2);
+         #print(plot(auxSet1));
+         #print(auxSet);
+         cellData[site] := [pt, produceCycle(pt, auxSet)];
+         #print(plot(cellData[site][2]));
       end proc;
 
       generateCells := proc()
-         
-         for s in vertices do
+         for s in sitesOfDiagram do
             generateCell(s);
          od;
       end proc;
 
+      # the output will be [P1,P2,P3] P1 is the cannonical pt of the diagram, P2 is an ordered tuple of sites ordered by angle of cannonical-site, P3 is the table of cell data, P4 is the complete cycle table
+      
+      #we make use of maple-implimented algorithms. The graph theory package. To do this, we must associate each vertice of the diagram to a number--an ordering. We make a graph with no edges but |V| nodes each corresponding to an vert.
+      # from here we need to add the edges. Runtime analysis might be good here. We then have a graph with undirected. 
+      # to create the cycles, we take the cell data and concatenate it onto two other paths. After obtaining this data, we enumerate the sites according to ascending argument
       getData := proc()
-         generateCells();
+         local auxSet1, auxSet2, i, j, k, v, p1 := [], p2, p3, P:=[], Q :=[];
+         #print(plot(curves));
+         #intialization
+         generateCells(); # construct cell data
+         auxSet1 := convert( vertices, list ); # enumerate the list
+         v := canonicalVert( vertices );
+         G := Graph( nops(auxSet1) );
+         for e in edges do
+            i:=Search( e[1], auxSet1 );
+            j:=Search( e[2], auxSet1 );
+            AddEdge( G, {i,j} );
+         od;
+         #print("graph");
+         #print((DrawGraph(G, layout=planar)));
+         k:= Search( v, auxSet1 ); # convert the canonical one
+         #print(k);
+         for s in sitesOfDiagram do
+            i :=  Search( cellData[s][1], auxSet1 );
+          #  print(assigned(cellData[s]));
+           # print(cellData[s][1]);
+           # print(auxSet1);
+           # print({k,i});
+            auxSet2 := auxSet1[ ShortestPath( G, k, i) ];
+            for j from 1 to nops(auxSet2)-1 do
+               p1 := [op(p1), [ auxSet2[j], auxSet2[j+1] ] ];
+            od;
+            p2 := cellData[s][2];
+             print(display({plot({op(p1), op(p2)}, color="Red"), pointplot([v,s], symbol=solidcircle, color="Red")}));
+            p3 := Reverse(p1);
+            P := [ op(P), [op(p1), op(p2), op(p3)] ];
+           
+            Q := [op(Q), p2];
+            p1:=[];
+         od;
+         return [v, sitesOfDiagram, P, Q];
       end proc;
 
+
+  #NOT USED IN OUR IMPLIMENTATION
+      setRay := proc( x, y, dx, dy, s1, s2) 
+        # local mult;
+        # mult := range/evalf(sqrt(dx^2+dy^2)); 
+        # curves := curves union {[[x,y],[x+mult*dx,y+mult*dy]]};
+        return this;
+      end proc;
+      #NOT USED IN OUR IMPLIMENTATION
+      setLine := proc( x, y, dx, dy, s1, s2 )
+        # local mult;
+        # mult := range/evalf(sqrt(dx^2+dy^2)); # ensure the line extends to the edges
+        # curves := curves union {[[x-mult*dx,y-mult*dy],[x+mult*dx,y+mult*dy]]};
+         return this;
+      end proc;
    
+
+    
+
    end module;
 end proc:
-
-
--- Monodromy.mm
+++ Monodromy.mm
@@ -6,7 +6,7 @@
 Monodromy := proc(f, x, y, val1 :: posint := 10, showpath := true )
 	local FortunesAlgorithm, PriorityQueue, Data, Edge, Bisector, intersections, SweepLine,
 	#local vars
-		n := algcurves[genus](f, x, y), a_n, X, Y, f_y, discrimiant, auxSet1 := {}, j, problemPts := {}, problemPtsFloat := {}, auxSet2 := {}, N, centerOfData, r, boundarySites := {}, minimalDist, val2 := 1, aux_fun1, data, tempCell, edgeData := {}, indexer := 0; 
+		n := algcurves[genus](f, x, y), a_n, X, Y, disc, j, problemPts := {}, problemPtsFloat := {}, auxSet1 :={},  auxSet2 := {}, N, centerOfData, r, boundarySites := {}, minimalDist, val2 := 1, aux_fun1, data, tempCell, edgeData := {}, indexer := 0, A; 
 	uses ComputationalGeometry;
 		 
 $include "Voronoi-Monodromy\PriorityQueue.mm"	
@@ -17,10 +17,10 @@ $include "Voronoi-Monodromy\Bisector.mm"
 $include "Voronoi-Monodromy\Edge.mm"
 $include "Voronoi-Monodromy\Intersections.mm"
 
-	print("curve:");
-	print(f);
-	print("genus");
-	print(n);
+	#print("curve:");
+	#print(f);
+	#print("genus");
+	#print(n);
 
 	#Check that f is reasonable here
 	#1) is f of type ?
@@ -33,23 +33,23 @@ $include "Voronoi-Monodromy\Intersections.mm"
 	# Obtaining problem points Q: should f be normalized? Does that matter? Should we switch to algcurves here? 
 	#f_y:=diff(expand(f),y); 
 	a_n := expand( lcoeff(f, y) );
-	discrimiant :=expand( discrim(f, y) ); 
+	disc := expand( discrim(f, y) ); 
 	#print("f_y");
 	#print(f_y);
-	print("disc");
-	print( discrimiant );
-	print("a_n");
-	print( a_n );
+	#print("disc");
+	#print( disc );
+	#print("a_n");
+	#print( a_n );
 	# Solving discrimiant=0 External code impacts runtime here
-	print("exact problem points"); 
+	#print("exact problem points"); 
 
-	problemPts := allvalues~( { solve( discrimiant=0 ) } );
+	problemPts := allvalues~( { solve( disc=0 ) } );
 	problemPts := problemPts union allvalues~( { solve( a_n=0 ) } );
 
-	print( problemPts );
+	#print( problemPts );
 	
 	#convert to floats in R2
-	print("converting values to two dimensional floats. This may take time"	);
+	#print("converting values to two dimensional floats. This may take time"	);
 	 
 	auxSet1 := evalf[val1]~( problemPts );
 	for j in auxSet1 do
@@ -60,49 +60,49 @@ $include "Voronoi-Monodromy\Intersections.mm"
 		#print( "real" );
 		Y :=  Im( j );
 		#print( "im" );
-		problemPtsFloat := problemPtsFloat union { [ X,Y ] };
+		problemPtsFloat := problemPtsFloat union { [ X, Y ] };
 		auxSet2 := auxSet2 union { [ X, 0. ], [ 0., Y ] };
 	od;
 	
 	
 	#this is where accuracy becomes an issue. Q could we use a less explicit way to obtain these floats? This line seems redundant.
-	print("there are");
-	print( nops(problemPts) );
+	#print("there are");
+	#print( nops(problemPts) );
 	N := nops( problemPtsFloat );
-	print( N );
-	print("number of problem pts (both numbers should match above)");
-	print( problemPtsFloat );
+	#print( N );
+	#print("number of problem pts (both numbers should match above)");
+	#print( problemPtsFloat );
 
 	#CURRENT STATE: All problem points are processed. 
 
 	# compute center and boundary sites 
-	print("computing center of data and boundary region");
+	#print("computing center of data and boundary region");
 	
 	centerOfData := [ add( problemPtsFloat[k][1]/N, k=1..N ), add( problemPtsFloat[k][2]/N, k=1..N ) ];
 
 	aux_fun1 := (a,b) -> distance( [ a, b ], centerOfData );
 
-	print("Center:");
-	print( centerOfData );
+	#print("Center:");
+	#print( centerOfData );
 
 	#the radius can be larger than the max so lets ensure that it is atleast of the order of 10^0 QUESTION: does radius change this impact accuracy
 	r := 2*max(  evalf[val1]~( aux_fun1~( op(problemPtsFloat) ) ), 1  );
-	print("radius");
-	print( r );
+	#print("radius");
+	#print( r );
 
 	#we choose boundary sites at center + r*exp(j*pi/3) j=1..6
 	for j from 1 to 6 do
 		X := evalf[val1]( centerOfData[1] + r*cos( 1/3*Pi*j ) );
 		Y := evalf[val1]( centerOfData[2] + r*sin( 1/3*Pi*j ) );
 		boundarySites := boundarySites union { [ X, Y ] };
-		auxSet2 :=auxSet2 union {[X,0.], [0.,Y]};
+		auxSet2 := auxSet2 union { [ X, 0. ], [ 0., Y ] };
 	od;
 
-	print("accuracy needed is found by computing the minimal dist between x values and y values because we need to beable to distinguish when two components are not the same. This distance is:");
+	#print("accuracy needed is found by computing the minimal dist between x values and y values because we need to beable to distinguish when two components are not the same. This distance is:");
 	
 	#External code impacting runtime
 	minimalDist := ComputationalGeometry[ClosestPointPair]( convert( auxSet2, list ) )[1]; 
-	print(minimalDist);
+	#print(minimalDist);
 	#assuming that the dist is never zero.
 	while trunc(minimalDist)=0 do
 		minimalDist := minimalDist*10;
@@ -117,20 +117,25 @@ $include "Voronoi-Monodromy\Intersections.mm"
 
 
 	#apply the algorithm
-	print( "applying algorimthm now");
+	#print( "applying algorimthm now");
 	data := Data();
 	data :- setSites( problemPtsFloat );
 	## Get edges and verts in plane
-	data := FortunesAlgorithm( problemPtsFloat union boundarySites , data ):
+	data := FortunesAlgorithm( problemPtsFloat union boundarySites, data ):
 	
-	data:-getData();
+	A:=data:-getData():
+
+	H:={};
+	for e in A[3] do
+	H:= H union {op(e)};
+	od;
 	#print("The data is given below");
 	#for j in data:-vertices do
 	#	tempCell:=data:-cellData[j];
-	#	print(plots[display]({plot(tempCell,color="Red"),plots[pointplot]({j},symbol=solidcircle,color="Red")}));
+		print(plot(H, color="Red"));
 	#	edgeData:=edgeData union tempCell;
 	#od;
-	if showpath then
-		print( plots[display]( { plot( data:-edges, color="Red" ), plots[pointplot]( data:-vertices, symbol=solidcircle, color="Red") } ) );
-	fi;
+	#if showpath then
+	#	print( plots[display]( { plot( data:-edges, color="Red" ), plots[pointplot]( data:-vertices, symbol=solidcircle, color="Red") } ) );
+	#fi;
 end proc;
